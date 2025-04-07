fe09953ab3041650f7342a79152180f24cfb08f2
michaelflemingmanz <michaelflemingmanz@gmail.com>
2025-04-07 16:08:08 -0400

    Tu comentario explicando los cambios

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,1 +1,15 @@
-a
+function MyButton({ title }: { title: string }) {
+    return (
+      <button>{title}</button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Bienvenido a mi aplicación</h1>
+        <MyButton title="Soy un botón" />
+      </div>
+    );
+  }
+  
